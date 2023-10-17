29bb431845ef3fb5ca4942ac5d61fa1e01e428a5
Nahid <nahidbabu6@gmail.com>
2023-10-17 19:47:05 -0400

    Ullah vai please check the commit

--- src/test/java/util/FirstGitTest.java
+++ src/test/java/util/FirstGitTest.java
@@ -3,7 +3,6 @@ package util;
 public class FirstGitTest {
 
  /*   System.out.println("Testing the git in Intellij");
-   System.out.println("2nd test for git in Intellij");
-   System.out.println();
+
 */
 }
